d7b7ffef737d5512a351c10a1267d0bd6d9d709b
developElisheva <elisheva2107@gmail.com>
2025-03-11 14:24:23 +0200

    good!

--- Node project/index.js
+++ Node project/index.js
@@ -1,15 +1,29 @@
+import express from 'express';
 import axios from 'axios';
+
+const app = express();
+const port = 5005;
 
 const options = {
   method: 'GET',
-  url: 'https://api.render.com/v1/services?includePreviews=true&limit=20',
+  url: 'https://api.render.com/v1/services?includePreviews=true&limit=20', 
   headers: {
     accept: 'application/json',
-    authorization: 'Bearer rnd_WtV3pWjepr6v5ziUi3OL7xJakTTw'
+    authorization: 'Bearer rnd_WtV3pWjepr6v5ziUi3OL7xJakTTw' 
   }
 };
 
-axios
-  .request(options)
-  .then(res => console.log(res.data))
-  .catch(err => console.error(err));
+app.get('/apps', (req, res) => {
+  axios
+    .request(options)
+    .then(response => {
+      res.json(response.data); 
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error fetching data from Render API' });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
